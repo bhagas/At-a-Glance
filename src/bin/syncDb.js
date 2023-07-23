c407e2ebc6571e5ea6fd8dd0cd693f13542789cf
unknown <tump.store@gmail.com>
2023-07-23 18:11:09 +0700

    db sync reconfig

--- src/bin/syncDb.js
+++ src/bin/syncDb.js
@@ -2,25 +2,29 @@ import koneksi from'../config/koneksi.js';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
+// import model_fd from '../modules/freshdesk/model.js'
+try {
+    const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 let normalizedPath = path.join(__dirname, "../modules");
 // console.log(normalizedPath);
-fs.readdirSync(normalizedPath).forEach(function (file) {
-    let normalize = path.join(__dirname, "../modules/" + file);
-    fs.readdirSync(normalize).forEach(function (file2) {
-        if (file2 == "model.js") {
-            import(`../modules/${file}/model.js`)
-        }
-    });
-});
-
-koneksi.sync({ alter: true }).then(() => {
+let folder1 = fs.readdirSync(normalizedPath)
+for (let u = 0; u < folder1.length; u++) {
+    let normalize = path.join(__dirname, "../modules/" + folder1[u]);
+    let folder2 =  fs.readdirSync(normalize)
+    for (let i = 0; i < folder2.length; i++) {
+        if (folder2[i] == "model.js") {
+            let m=  await  import(`../modules/${folder1[u]}/model.js`)
+             }
+    }
+    
+}
+   
+    await koneksi.sync({ alter: true })
     console.log('Database Berhasil di Sinkronisasi')
     console.log('disconnecting...')
     process.exit(0);
-}).catch(e => {
-    console.log(e)
-});
+} catch (error) {
+    console.log(error)
+}
