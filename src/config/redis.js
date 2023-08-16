3d9f23540edbe38b96a753d37bc999f347c76ab9
unknown <tump.store@gmail.com>
2023-08-16 21:24:10 +0700

    change redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,6 +4,8 @@ import Redis from 'ioredis';
 const options = {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    db: process.env.REDIS_DB,
     retryStrategy: times => {
       // reconnect after
       return Math.min(times * 50, 2000);
