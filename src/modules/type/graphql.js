2f5f1cd86caa7690f3a2cf6c9182057fac3819d7
zakka <mzakka163@gmail.com>
2023-09-02 08:33:29 +0700

    add model type & graph type

--- src/modules/type/graphql.js
+++ src/modules/type/graphql.js
@@ -1,132 +1,105 @@
-import db from'../../config/koneksi.js';
-import { QueryTypes } from'sequelize';
-import qwModel from'./model.js';
-import gql from'graphql-tag';
-import { v4 as uuidv4 } from'uuid';
-import moment from 'moment';
-import { getMimeType } from 'stream-mime-type';
-import * as XLSX from 'xlsx/xlsx.mjs';
-
-const typeDefs=
+import db from '../../config/koneksi.js';
+import typeModel from './model.js'
+import { QueryTypes } from 'sequelize';
+import gql from 'graphql-tag';
+import { v4 as uuidv4 } from 'uuid';
+const typeDefs =
   gql`
-  extend type Query {
-      types: quoteWorksResult
-      typeById(id: ID!): quoteWorksDetailResult
-     
+  extend type Query{
+    types:typesResult
+    type(id: ID!):Type
   }
 
-  extend type Mutation {
- 
- updateQuoteWorks(id:ID!, input:qwInput): Output
- importQuoteWorks(filee:[Upload]): Output
-
-}
-
-input qwInput {
-    number:String,
-    name:String,
-    sold_to_company:String,
-    opp_date:String,
-    opp_stage:String,
-    sales_rep:String,
-    total_amount:String,
-    cust_po_number:String,
-    est_close_date:String,
-    created:String,
-    pnx_engineer:String,
-    line_of_business:String,
-    doc_status_date:String,
-    root_cause:String,
-    sold_to_contact:String,
-    preparer:String,
-    ship_to_company:String,
-    bill_to_company:String
+  type typesResult{
+    data:[Type],
+    message:String,
+    status:Int
   }
-type quoteWorksResult{
-  data:[quoteWork],
-  message:String,
-  status:Int
-}
-type quoteWorksDetailResult{
-  data:quoteWork,
-  message:String,
-  status:Int
-}
-  
-
-  type quoteWork {
+  type Type {
     id: ID!,
-    number:String,
-    name:String,
-    sold_to_company:String,
-    opp_date:String,
-    opp_stage:String,
-    sales_rep:String,
-    total_amount:String,
-    cust_po_number:String,
-    est_close_date:String,
-    created:String,
-    pnx_engineer:String,
-    line_of_business:String,
-    doc_status_date:String,
-    root_cause:String,
-    sold_to_contact:String,
-    preparer:String,
-    ship_to_company:String,
-    bill_to_company:String
-  }
-`
-
-const resolvers= {
+    type_name: String,
+    createdAt: String,
+    updatedAt:String
+ }
+ extend type Mutation{
+  createType(inputType: TypeInput): Output
+  updateType(id: ID!, input: TypeInput): Output
+  deleteType(id: ID!): Output
+ }
+ input TypeInput{
+  type_name:String
+ }
+  `
+const resolvers = {
   Query: {
-    quoteWorks: async (obj, args, context, info) => {
-try {
-    let dt = await db.query('select * from qw_opportunity where deleted is null');
-    // console.log(dt);
-    //bisa array return nya
-    return {data: dt[0], status:200, message:'Success'};
-     
-} catch (error) {
-    console.log(error);
-}
-   
+    types: async (obj, args, context, info) => {
+      let dt = await db.query('select * from types where deleted is null')
+      // console.log(dt);
+      return { data: dt[0], status: 200, message: 'Success' };
     },
-    typeById: async (obj, args, context, info) => {
-        try {
-            let dt = await db.query('select * from qw_opportunity where deleted is null and number=$number',{bind:{number:args.id}} );
-            // console.log(dt);
-            //bisa array return nya
-            if(dt[0]){
-                return {data: dt[0][0], status:200, message:'Success'};
-            }else{
-                return {data: {}, status:200, message:'Success'};
-            }
-       
-             
-        } catch (error) {
-            console.log(error);
+    type: async (obj, args, context, info) => {
+      console.log("get type");
+      let dt = await db.query(`select * from types where id= $1`, { bind: [args.id], type: QueryTypes.SELECT })
+      // console.log(dt);
+      return dt[0];
+    }
+  },
+  Mutation: {
+    createType: async (_, { inputType }) => {
+      try {
+        inputType.id = uuidv4()
+        console.log(inputType);
+        await typeModel.create(inputType)
+        return {
+          status: '200',
+          message: 'Success'
         }
-           
-            },
-},
-Mutation:{
-    updateQuoteWorks: async (obj, {id, input}, context, info) => {
-        try {
-            await qwModel.update(
-                input,
-                 { where: { id } }
-               )
-            return {status:200, message:'Success'};
-             
-        } catch (error) {
-            console.log(error);
-            return {status:200, message:'Failed'};
+      } catch (error) {
+        console.log(error);
+        return {
+          status: '500',
+          message: 'Failed',
+          error: JSON.stringify(error)
         }
-           
-            },
-  
-}
-}
+      }
+    },
+    updateType: async (_, { id, input }) => {
+      try {
+        await typeModel.update(
+          input,
+          { where: { id } }
+        )
+        return {
+          status: '200',
+          message: 'Updated'
+        }
+      } catch (error) {
+        console.log(error);
+        return {
+          status: '500',
+          message: 'Failed',
+          error: JSON.stringify(error)
+        }
+      }
+    },
+    deleteType: async (_, { id }) => {
+      try {
+        await typeModel.destroy(
+          { where: { id } }
+        )
+        return {
+          status: '200',
+          message: 'Removed'
+        }
+      } catch (error) {
+        return {
+          status: '500',
+          message: 'Internal Server Error',
+          error: JSON.stringify(error)
+        }
+      }
+    }
 
-
-export {typeDefs, resolvers}
+  }
+}
+export { typeDefs, resolvers }
