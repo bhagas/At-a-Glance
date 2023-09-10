808f92095e5f5f51b1da68ac1d7d0b3d8027712d
zakka <mzakka163@gmail.com>
2023-09-10 18:42:02 +0700

    model fd_agent_member

---
+++ src/modules/fd_agent_member/model.js
@@ -0,0 +1,39 @@
+import {DataTypes, ENUM } from'sequelize';
+import koneksi from'../../config/koneksi.js';
+import freshdesk_agents from'../freshdesk_agents/model.js'
+import user from '../user/model.js'
+const fd_agent_member = koneksi.define('fd_agent_member', {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.STRING,
+        primaryKey: true
+      },
+
+  }, {
+    // Other model options go here
+    freezeTableName: true,
+    paranoid:true,
+    deletedAt: 'deleted'
+  });
+
+  freshdesk_agents.hasMany(fd_agent_member,{
+    foreignKey: 'id_agent'
+  });
+  fd_agent_member.belongsTo(freshdesk_agents,{
+    foreignKey: 'id_agent'
+  });
+
+  user.hasMany(fd_agent_member,{
+    foreignKey: 'id_member'
+  });
+  fd_agent_member.belongsTo(user,{
+    foreignKey: 'id_member'
+  });
+
+  user.hasMany(fd_agent_member,{
+    foreignKey: 'id_user_agent'
+  });
+  fd_agent_member.belongsTo(user,{
+    foreignKey: 'id_user_agent'
+  });
+export default fd_agent_member;
