9a260a70c28be162ec203194484a60862bdf14c4
bhagas koro <tump.store@gmail.com>
2023-10-27 18:48:35 +0700

    users

--- src/modules/ticket_member/graphql.js
+++ src/modules/ticket_member/graphql.js
@@ -54,12 +54,22 @@ Mutation:{
   
     addMemberToTicket: async(_, {idAgent, idUserAgent, idUserMember, fd_ticket_id}, context)=>{
     try {
-    
-      await ticket_member.create({id: uuidv4(),id_agent: idAgent, id_member:idUserMember, id_user_agent:idUserAgent,fd_ticket_id, created_by:context.user_app.id})
-      return {
-        status: '200',
-        message: 'success'
-    }
+      let dt = await db.query('select a.*, b.name as "memberName" from ticket_member a join users b on a.id_member  = b.id where a."deletedAt" is null and a.fd_ticket_id=$1',{bind: [fd_ticket_id],type: QueryTypes.SELECT});
+      if(dt.length){
+
+        await ticket_member.create({id: uuidv4(),id_agent: idAgent, id_member:idUserMember, id_user_agent:idUserAgent,fd_ticket_id, created_by:context.user_app.id})
+        return {
+          status: '200',
+          message: 'success'
+      }
+      }else{
+        return {
+          status: '500',
+          message: 'Member already registered!',
+          
+      }
+      }
+     
     } catch (error) {
       return {
         status: '500',
